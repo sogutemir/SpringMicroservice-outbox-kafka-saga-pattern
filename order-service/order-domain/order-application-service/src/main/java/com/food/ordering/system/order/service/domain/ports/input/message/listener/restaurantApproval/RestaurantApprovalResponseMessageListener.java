f5e56fc29ad5a78bbe7563fdcad7d623cbe347a3
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-09-17 12:11:16 +0300

    Fix bugs

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restaurantApproval/RestaurantApprovalResponseMessageListener.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restaurantApproval/RestaurantApprovalResponseMessageListener.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantApproval;
+package com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval;
 
 import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
 
--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurantApproval/OrderPaidRestaurantRequestMessagePublisher.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurantApproval/OrderPaidRestaurantRequestMessagePublisher.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurantApproval;
+package com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurantapproval;
 
 import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
